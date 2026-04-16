7f8db4d5737d03f2b7902ac3d6933caea16587d5
Théo Boiteux <theobtx987@gmail.com>
2026-04-16 14:02:04 +0200

    Tiny variable name correction

--- src/troll_rcpp.cpp
+++ src/troll_rcpp.cpp
@@ -4244,7 +4244,7 @@ void trollCpp(
   if (_MinLAImax == 1)
     Rcout << "Activated Module: MinLAImax" << endl;
   if (_MaxLAImax == 1)
-    Rcout << "Activated Module: MaxLAImax ADXACSYBZJHBZJHEBKHE3BJHCBEJBCEJ" << endl;
+    Rcout << "Activated Module: MaxLAImax" << endl;
   if (_CustomPhenology == 1)
     Rcout << "Activated Module: CustomPhenology" << endl;
 
