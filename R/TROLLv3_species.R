c98aac27320217d2565f5cf44ef72cc9afaefb98
Théo Boiteux <theobtx987@gmail.com>
2026-04-20 11:11:50 +0200

    trying to correct species table reading error

--- R/TROLLv3_species.R
+++ R/TROLLv3_species.R
@@ -8,7 +8,8 @@
 #'   \item{s_wsg}{wood specific gravity} \item{s_dbhmax}{maximum diameter}
 #'   \item{s_hmax}{maximum height} \item{s_ah}{height-diameter allometry
 #'   coefficient} \item{s_regionalfreq}{regional frequency} \item{s_tlp}{Turgor
-#'   loss point} \item{s_drymass}{leaf dry mass} \item{s_seedmass}{seed mass} } 
+#'   loss point} \item{s_drymass}{leaf dry mass} \item{s_seedmass}{seed mass} \item{s_seedlingCycle}{seedling cycle} 
+#'   \item{s_seedlingOffset}{seedling offset} \item{s_randomCycle}{random cycle} \item{s_randomOffset}{random offset} } 
 #'
 #' @seealso [troll()], [stack()]
 #'
